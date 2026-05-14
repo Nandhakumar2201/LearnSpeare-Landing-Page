4d97634752e35a5e89971f50f1013ee3a91953ac
nandha <nalliayanandhakumar@gmail.com>
2026-05-14 20:02:25 +0530

    Add missing React imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import LandingPage from './pages/LandingPage';
 
 function App() {
